cad5290ac89c0b9c59a19cfcbad52d74eaa13349
Александр Нейчев <alexnejchev73@gmail.com>
2019-04-06 14:55:17 +0400

    Added UnitTestExporter and UnitTestParser in IntegratedTheDeanHelpers

--- IntegratedTestTheDeanHelpers/UnitTestExpoter.cs
+++ IntegratedTestTheDeanHelpers/UnitTestExpoter.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace IntegratedTestTheDeanHelpers
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTestExpoter
     {
         [TestMethod]
         public void TestMethod1()
---
+++ IntegratedTestTheDeanHelpers/UnitTestParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntegratedTestTheDeanHelpers
+{
+    [TestClass]
+    public class UnitTestParser
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
