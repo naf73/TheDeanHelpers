e3229f24b770e5dc19457d052a99015678069e50
Александр Нейчев <alexnejchev73@gmail.com>
2019-04-06 14:40:59 +0400

    Added Exporter and Parser. Also added library OpenXML.

---
+++ TheDeanHelpers/Parser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheDeanHelpers.Model;
+
+namespace TheDeanHelpers
+{
+    public static class Parser
+    {
+        public static CSVFile doc = new CSVFile();
+
+        
+    }
+}
