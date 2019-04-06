9ad5170b5cd5b9f05716c36c91e4b7a39d47a049
Александр Нейчев <alexnejchev73@gmail.com>
2019-04-06 14:49:11 +0400

    Modified Exporter from static to usually

--- TheDeanHelpers/Expoter.cs
+++ TheDeanHelpers/Expoter.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheDeanHelpers.Model;
 
 namespace TheDeanHelpers
 {
-    public static class Expoter
+    public class Expoter
     {
-
+        public void ExportToFileXLSX(string path, CSVFile csv)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
