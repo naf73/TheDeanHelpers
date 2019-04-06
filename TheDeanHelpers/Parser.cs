9e91ae60e61cc3ed036993a594757d915f72873c
snt23skv <snt23skv@hotmail.com>
2019-04-06 17:02:12 +0400

    added in class Parser method Download v1

--- TheDeanHelpers/Parser.cs
+++ TheDeanHelpers/Parser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,61 @@ namespace TheDeanHelpers
 {
     public  class Parser
     {
-        private  CSVFile doc = new CSVFile();
+        public CSVFile Download (string filePath )
+        {
+            if (!File.Exists(filePath))throw new FileNotFoundException();
+            using (var reader = new StreamReader(filePath, Encoding.Default))
+            {
+                string line;
+                bool firstLine = true;
+                CSVFile doc = new CSVFile();
+                int countRow = 0;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (firstLine)
+                    {
+                        int countColumns = 0;
+                        foreach (var word in line.Split(';'))
+                        {
+
+                            doc.Columns.Add(new Column()
+                            {
+                                Id = countColumns++,
+                                Name = word,
+                                IsActive = true
+                            });
+                        }
+                        firstLine = false;
+                    }
+                    else
+                    {
+                        Row r = new Row()
+                        {
+                            Id = 0
+                        };
+                        int countWord = 0;                        
+                        foreach (var word in line.Split(';'))
+                        {
+
+                            r.Cells.Add(new Cell()
+                            {
+                                ColumnId = countWord,
+                                RowId = countRow,
+                                Value=word
+                            });
+                            countWord++;
+                        }
+                        doc.Rows.Add(r);
+                        countRow++;
+                    }
+                }
+                return doc;
+            }
+            
+            
+            
+        }
+
 
         
     }
