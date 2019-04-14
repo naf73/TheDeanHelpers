1b510030e684e887857b86405e64571ef890b4b3
Александр Нейчев <alexnejchev73@gmail.com>
2019-04-14 13:41:05 +0400

    Added checkbox in headers

--- TheDeanHelpers/MainWindow.xaml.cs
+++ TheDeanHelpers/MainWindow.xaml.cs
@@ -26,7 +26,6 @@ namespace TheDeanHelpers
     {
         #region Variables
 
-        private DataTable doc = new DataTable();
         Parser parser = new Parser();
         Expoter expoter = new Expoter();
 
@@ -52,13 +51,51 @@ namespace TheDeanHelpers
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void OpenCSV_Click(object sender, RoutedEventArgs e)
-        {            
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "CSV file|*.csv";
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "CSV file|*.csv"
+            };
             if (openFileDialog.ShowDialog() == true)
             {
                 DataGridTable.DataContext = parser.Download(openFileDialog.FileName);
+                
+                foreach (DataGridColumn column in DataGridTable.Columns)
+                {
+                    DataTemplate dataTemplate = new DataTemplate();
+
+                    FrameworkElementFactory _template = new FrameworkElementFactory(typeof(StackPanel));
+                    _template.SetValue(StackPanel.OrientationProperty, Orientation.Horizontal);
+
+                    FrameworkElementFactory _firstTemplateChild = new FrameworkElementFactory(typeof(CheckBox));
+                    _firstTemplateChild.SetValue(CheckBox.IsThreeStateProperty, false);
+                    _firstTemplateChild.SetValue(CheckBox.IsCheckedProperty, true);
+                    _firstTemplateChild.SetValue(CheckBox.NameProperty, string.Format("checkBox_{0}", column.DisplayIndex));
+                    _firstTemplateChild.AddHandler(CheckBox.ClickEvent, new RoutedEventHandler(CheckBox_Click));
+
+                    FrameworkElementFactory _secondTemplateChild = new FrameworkElementFactory(typeof(Label));
+                    _secondTemplateChild.SetValue(Label.NameProperty, "label");
+                    _secondTemplateChild.SetValue(Label.ContentProperty, column.Header);
+                    _secondTemplateChild.AddHandler(Label.MouseDoubleClickEvent, new MouseButtonEventHandler(Header_Click));
+
+                    _template.AppendChild(_firstTemplateChild);
+                    _template.AppendChild(_secondTemplateChild);
+
+                    dataTemplate.VisualTree = _template;
+                    column.HeaderTemplate = dataTemplate;
+                }
             }
+        }
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            CheckBox checkBox = (CheckBox)sender;
+            MessageBox.Show(checkBox.IsChecked.ToString());
+        }
+
+        private void Header_Click(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show(((Label)sender).Content.ToString());
         }
 
         /// <summary>
@@ -68,24 +105,70 @@ namespace TheDeanHelpers
         /// <param name="e"></param>
         private void ExportXLS_Click(object sender, RoutedEventArgs e)
         {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "Excel file|*.xls|Excel file|*.xlsx";
-            if(saveFileDialog.ShowDialog() == true)
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Excel file|*.xls|Excel file|*.xlsx"
+            };
+            if (saveFileDialog.ShowDialog() == true)
             {
                 DataTable table = new DataTable();
                 table = (DataTable)DataGridTable.DataContext;
                 foreach (DataGridColumn column in DataGridTable.Columns)
                 {
+                    StackPanel stackPanel = column.HeaderTemplate.LoadContent() as StackPanel;
+                    CheckBox box = FindVisualChildByName<CheckBox>(stackPanel, string.Format("checkBox_{0}", column.DisplayIndex));
+                    Label label = FindVisualChildByName<Label>(stackPanel, "label");
+
+                    if (box.IsChecked == true)
+                    {
+                        MessageBox.Show(label.Content.ToString());
+                    }
+
+                    foreach (var item in stackPanel.Children)
+                    {
+                        if (item is CheckBox)
+                        {
+                            MessageBox.Show(string.Format("{0} {1}", ((CheckBox)item).Name, ((CheckBox)item).IsChecked));
+                            if (((CheckBox)item).IsChecked == false)
+                            {
+                                MessageBox.Show(column.Header.ToString());
+                            }
+                        }
+                    }
+
+                    // -- --
+
                     table.Columns[(string)column.Header].SetOrdinal(column.DisplayIndex);
                 }
                 expoter.ExportToFileXLSX(saveFileDialog.FileName, table);
+                MessageBox.Show("Экпорт завершен");
             }
-            MessageBox.Show("Экпорт завершен");
         }
 
         #endregion
 
         #region Methods
+
+        public static T FindVisualChildByName<T>(DependencyObject parent, string name) where T : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                string controlName = child.GetValue(Control.NameProperty) as string;
+                if (controlName == name)
+                {
+                    return child as T;
+                }
+                else
+                {
+                    T result = FindVisualChildByName<T>(child, name);
+                    if (result != null)
+                        return result;
+                }
+            }
+            return null;
+        }
+
 
         #endregion
     }
